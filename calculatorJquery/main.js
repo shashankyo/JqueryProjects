1fa8547858ca238ca1e6ea5810045d351f675937
shashank <shashank944533@gmail.com>
2023-02-02 07:08:11 +0530

    first commit

---
+++ calculatorJquery/main.js
@@ -0,0 +1,16 @@
+function insrt(num){
+    $('.calc-display').val($('.calc-display').val() + num);
+
+}
+function eql(){
+     $('.calc-display').val(eval($('.calc-display').val()));
+}
+
+function C(){
+    $('.calc-display').val('');
+}
+
+function del(){
+    value=$('.calc-display').val();
+    $('.calc-display').val(value.substring(0, value.length - 1 ));
+}
